a68c8561c111c2c22a2cf6f3f4671c1834716794
DexHolder04 <53274549+DexHolder04@users.noreply.github.com>
2020-10-21 17:19:29 -0600

    Deerling frameshift Notice

--- js/buildTable.js
+++ js/buildTable.js
@@ -1,6 +1,6 @@
 let migrationDate = '2020-10-29T00:00:00Z';
-let noticeText = "With the start of the <strong>Autumn Event</strong> current nesting species have been disrupted. <strong>Some</strong> nests have changed, so keep that in mind while you browse our report. We'll update it as soon as possible!";
-let notice = false;
+let noticeText = "With the end of the <strong>Autumn Event</strong> Deerling has been removed from the nest pool. <strong>Some</strong> nests have changed, so keep that in mind while you browse our report. We'll update it as soon as possible!";
+let notice = true;
 
 function buildHtmlTable() {
     let tableData = nestData;
