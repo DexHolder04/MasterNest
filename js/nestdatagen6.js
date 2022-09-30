6b8b91f7357f6d0333ee699e80c75444ad8aecf7
DexHolder04 <themasternest@gmail.com>
2022-09-30 02:41:23 -0600

    Migration 186 Report

--- js/nestdatagen6.js
+++ js/nestdatagen6.js
@@ -5,10 +5,10 @@ var nestData = [
     "DexNo": 661,
     "DexString": "661",
     "Shiny": true,
-    "Coordinates": "34.7148,137.6033",
-    "Locale": "Shizuoka, Japan",
-    "Timestamp": 1663381770745,
-    "GPX": "fletchling_34.7148,137.6033",
+    "Coordinates": "24.0258,-104.6869",
+    "Locale": "Durango, Mexico",
+    "Timestamp": 1664502828875,
+    "GPX": "fletchling_24.0258,-104.6869",
     "Form": "normal"
   },
   {
@@ -17,10 +17,10 @@ var nestData = [
     "DexNo": 661,
     "DexString": "661",
     "Shiny": true,
-    "Coordinates": "36.9016,-76.2002",
-    "Locale": "Virginia, USA",
-    "Timestamp": 1663387369882,
-    "GPX": "fletchling_36.9016,-76.2002",
+    "Coordinates": "34.0349,-118.0570",
+    "Locale": "California, USA",
+    "Timestamp": 1664503690174,
+    "GPX": "fletchling_34.0349,-118.0570",
     "Form": "normal"
   },
   {
@@ -29,10 +29,10 @@ var nestData = [
     "DexNo": 661,
     "DexString": "661",
     "Shiny": true,
-    "Coordinates": "40.6535,-74.2461",
-    "Locale": "New Jersey, USA",
-    "Timestamp": 1663389805230,
-    "GPX": "fletchling_40.6535,-74.2461",
+    "Coordinates": "34.6516,135.5094",
+    "Locale": "Osaka, Japan",
+    "Timestamp": 1664504094261,
+    "GPX": "fletchling_34.6516,135.5094",
     "Form": "normal"
   },
   {
@@ -41,22 +41,10 @@ var nestData = [
     "DexNo": 661,
     "DexString": "661",
     "Shiny": true,
-    "Coordinates": "44.4695,26.0844",
-    "Locale": "Bucharest, Romania",
-    "Timestamp": 1663396870661,
-    "GPX": "fletchling_44.4695,26.0844",
-    "Form": "normal"
-  },
-  {
-    "Pokemon": "Fletchling",
-    "Gen": 6,
-    "DexNo": 661,
-    "DexString": "661",
-    "Shiny": true,
-    "Coordinates": "52.2132,21.0340",
-    "Locale": "Warsaw, Poland",
-    "Timestamp": 1663399273537,
-    "GPX": "fletchling_52.2132,21.0340",
+    "Coordinates": "36.3727,140.2634",
+    "Locale": "Ibaraki, Japan",
+    "Timestamp": 1664513234787,
+    "GPX": "fletchling_36.3727,140.2634",
     "Form": "normal"
   }
 ];
