c663dc7ad1e6fee8208a728b03b16c1e8b18cc65
DexHolder04 <themasternest@gmail.com>
2022-11-12 03:55:30 -0600

    Migration 189 Report

--- js/nestdatagen6.js
+++ js/nestdatagen6.js
@@ -5,10 +5,10 @@ var nestData = [
     "DexNo": 659,
     "DexString": "659",
     "Shiny": true,
-    "Coordinates": "-41.3045,174.7906",
-    "Locale": "Wellington, New Zealand",
-    "Timestamp": 1666394778155,
-    "GPX": "bunnelby_-41.3045,174.7906",
+    "Coordinates": "-20.4523,-54.5707",
+    "Locale": "Mato Grosso do Sul, Brazil",
+    "Timestamp": 1668046005382,
+    "GPX": "bunnelby_-20.4523,-54.5707",
     "Form": "normal"
   },
   {
@@ -17,46 +17,10 @@ var nestData = [
     "DexNo": 661,
     "DexString": "661",
     "Shiny": true,
-    "Coordinates": "29.9853,-90.0966",
-    "Locale": "Louisiana, USA",
-    "Timestamp": 1666408505194,
-    "GPX": "fletchling_29.9853,-90.0966",
-    "Form": "normal"
-  },
-  {
-    "Pokemon": "Fletchling",
-    "Gen": 6,
-    "DexNo": 661,
-    "DexString": "661",
-    "Shiny": true,
-    "Coordinates": "39.9835,-75.2104",
-    "Locale": "Pennsylvania, USA",
-    "Timestamp": 1666489798284,
-    "GPX": "fletchling_39.9835,-75.2104",
-    "Form": "normal"
-  },
-  {
-    "Pokemon": "Fletchling",
-    "Gen": 6,
-    "DexNo": 661,
-    "DexString": "661",
-    "Shiny": true,
-    "Coordinates": "41.8762,-87.6197",
-    "Locale": "Illinois, USA",
-    "Timestamp": 1666490980844,
-    "GPX": "fletchling_41.8762,-87.6197",
-    "Form": "normal"
-  },
-  {
-    "Pokemon": "Fletchling",
-    "Gen": 6,
-    "DexNo": 661,
-    "DexString": "661",
-    "Shiny": true,
-    "Coordinates": "49.3015,-123.1334",
-    "Locale": "British Columbia, Canada",
-    "Timestamp": 1666515289143,
-    "GPX": "fletchling_49.3015,-123.1334",
+    "Coordinates": "8.9976,-79.5084",
+    "Locale": "Panama City, Panama",
+    "Timestamp": 1668245904782,
+    "GPX": "fletchling_8.9976,-79.5084",
     "Form": "normal"
   }
 ];
