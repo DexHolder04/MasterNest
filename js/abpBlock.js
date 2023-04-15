4543dfdf3d0d72b7a69b7ce3149f86a3037a562a
DexHolder04 <themasternest@gmail.com>
2023-04-14 22:05:31 -0600

    PayPal button update

--- js/abpBlock.js
+++ js/abpBlock.js
@@ -11,8 +11,8 @@ function noAds() {
         paypalTableRow = document.createElement("tr"),
         paypalTableCell1 = document.createElement("td"),
         paypalTableCell2 = document.createElement("td"),
-        paypalButton = `<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank" style="display:inline-block"><input type="hidden" name="cmd" value="_s-xclick"/><input type="hidden" name="hosted_button_id" value="C5CLJ64PC83ES"/><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button"/><img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1"/></form>`,
-        paypalMessage = `<strong>No ads? No problem!</strong> You can support the Master Nest by <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=C5CLJ64PC83ES&source=url">donating through PayPal</a>. Thanks for visiting us!`;
+        paypalButton = `<form action="https://www.paypal.com/donate" method="post" target="_top"><input type="hidden" name="hosted_button_id" value="C5CLJ64PC83ES" /><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" /><img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" /></form>`,
+        paypalMessage = `<strong>No ads? No problem!</strong> You can support the Master Nest by <a href="https://www.paypal.com/donate/?hosted_button_id=C5CLJ64PC83ES">donating through PayPal</a>. Thanks for visiting us!`;
     paypalTableCell1.innerHTML = paypalButton;
     paypalTableCell2.innerHTML = paypalMessage;
     paypalTableRow.innerHTML = paypalTableCell1.outerHTML + paypalTableCell2.outerHTML;
